949b346b6e1487523963d2826488dfc0d1502dde
chiranjeevi <chiranjeevi.chouvhan8004@gmail.com>
2019-01-10 18:02:25 +0530

    updated header section for all pages

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,4 +10,10 @@ $(document).ready(function() {
         // $(".tab").addClass("active"); // instead of this do the below 
         $(this).addClass("active2");   
     });
+
+    $(".tab3").click(function () {
+        $(".tab3").removeClass("active");
+        // $(".tab").addClass("active"); // instead of this do the below 
+        $(this).addClass("active");   
+    });
 });
